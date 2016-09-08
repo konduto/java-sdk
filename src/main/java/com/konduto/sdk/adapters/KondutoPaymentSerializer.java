f620e7d30435b7f25495a2ded16e6f35228c23cb
Raphael Sampaio <raphael@konduto.com>
2016-09-08 17:24:44 -0300

    fixes javadoc error

--- src/main/java/com/konduto/sdk/adapters/KondutoPaymentSerializer.java
+++ src/main/java/com/konduto/sdk/adapters/KondutoPaymentSerializer.java
@@ -17,18 +17,11 @@ import java.lang.reflect.Type;
  */
 public class KondutoPaymentSerializer implements JsonSerializer<KondutoPayment> {
     /**
-     * Gson invokes this call-back method during serialization when it encounters a field of the
-     * specified type.
-     * <p/>
-     * <p>In the implementation of this call-back method, you should consider invoking
-     * {@link JsonSerializationContext#serialize(Object, Type)} method to create JsonElements for any
-     * non-trivial field of the {@code src} object. However, you should never invoke it on the
-     * {@code src} object itself since that will cause an infinite loop (Gson will call your
-     * call-back method again).</p>
+     * KondutoPayment serializer
      *
-     * @param src       the object that needs to be converted to Json.
+     * @param payment   the payment that needs to be converted to Json.
      * @param typeOfSrc the actual type (fully genericized version) of the source object.
-     * @param context
+     * @param context   the serialization context
      * @return a JsonElement corresponding to the specified object.
      */
     @Override
